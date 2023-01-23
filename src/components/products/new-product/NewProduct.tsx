4218227ce423dd5ed5cf93923795cb78464222c3
yurimovo <yuri22091983@gmail.com>
2023-01-24 00:30:40 +0600

    Add product card sceleton

---
+++ src/components/products/new-product/NewProduct.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewProduct = () => {
+  return (
+    <div>
+      New product
+    </div>
+  );
+};
+
+export default NewProduct;
--- src/components/products/product-card/ProductCard.tsx
+++ src/components/products/product-card/ProductCard.tsx
@@ -1,11 +1,33 @@
 import React from "react";
+import { Button, TextField } from "@mui/material";
+
+import "./style.scss";
+import ProductImg from "../../../media/productImg.jpg";
 
 const ProductCard = () => {
-    return(
-        <div>
-            Product card
+  return(
+    <div className='productCard'>
+      <div className='productPhotoContainer'>
+        <img className='productPhoto' src={ProductImg} alt="Фото продукта" />
+      </div>
+      <div className='cardContent'>
+        <div className='productName'>
+          Header
         </div>
-    )
+        <div className='productPrice'>
+          1999.99
+        </div>
+        <div className='productQuantity'>
+          <div className='valueChangeButton'>-</div>
+          <input className='productQuantityField' id="outlined-basic" />
+          <div className='valueChangeButton'>+</div>
+        </div>
+        <div className='addToCartButtonContainer'>
+          <Button className='addToCartButton' variant="contained" color='secondary'>В корзину</Button>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default ProductCard;
